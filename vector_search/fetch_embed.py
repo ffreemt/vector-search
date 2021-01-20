3a4b15513950e8266182396995a1047da0a7fbd4
freemt <yucongo+fmt@gmail.com>
2021-01-20 21:21:14 +0800

    update fetch_embed

--- vector_search/fetch_embed.py
+++ vector_search/fetch_embed.py
@@ -2,6 +2,7 @@
 
 from typing import List, Optional, Union
 
+import numpy as np
 import httpx
 import logzero
 from logzero import logger
@@ -10,25 +11,30 @@ logzero.loglevel(10)
 
 URL = "http://127.0.0.1:8000/text/"
 URL = "http://216.24.255.63:8000/text/"
+URL = "http://216.24.255.63:8008/embed"
 
 
 # fmt: off
-def fetch_sent_corr(
+def fetch_embed(
         sents1: Union[str, List[str]],
-        sents2: Optional[Union[str, List[str]]] = None,
         url: str = URL,
 ) -> Optional[List[List[str]]]:
     # fmt: on
-    """ fetch doc/para/send corr matrix from fastai server. """
+    """Fetch doc/para/send embedding from fastai server.
+
+    >>> sents1 = ["test abc", "test abc"]
+    >>> res = fetch_embed(sents1)
+    >>> np.array(res).shape ==  (2, 512)
+    True
+    >>> res[0] == res[1]
+    True
+    """
 
     if isinstance(sents1, str):
         sents1 = [sents1]
-    if isinstance(sents2, str):
-        sents2 = [sents2]
 
     data = {
         "text1": sents1,
-        "text2": sents2,
     }
     try:
         resp = httpx.post(
@@ -47,8 +53,7 @@ def fetch_sent_corr(
         jdata = resp.json()  # type: ignore
     except Exception as exc:
         logger.error("exc: %s", exc)
-        jdata.update({"corr": [[0]]})
+        jdata.update({"embed": [[0]]})
         raise
 
-    return jdata.get("corr")
-
+    return jdata.get("embed")
